2a7cd1ce4225ace1d10b1251106a11d76833af75
Luke Partridge <lukej.partridge@icloud.com>
2025-04-29 19:12:06 +0100

    Basic Func

---
+++ script.js
@@ -0,0 +1,22 @@
+const game = new Chess();
+const board = Chessboard('board', {
+  draggable: true,
+  position: 'start',
+  onDrop: function (source, target) {
+    const move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q'
+    });
+
+    if (move === null) return 'snapback';
+  },
+  onSnapEnd: function () {
+    board.position(game.fen());
+  }
+});
+
+function startNewGame() {
+  game.reset();
+  board.start();
+}

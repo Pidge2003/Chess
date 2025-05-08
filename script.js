b411bad65aca048a92e73fc16a06ac81e856dad4
Luke Partridge <lukej.partridge@icloud.com>
2025-05-08 18:24:08 +0100

    Adding comments

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Select all elements with the class 'square' and add a click event listener to each
 document.querySelectorAll('.square').forEach((square, index) => {
     square.addEventListener('click', () => {
+      // Remove 'highlight' class from all squares
       clearHighlights();
+  
+      // Add 'highlight' class to the clicked square
       square.classList.add('highlight');
+  
+      // Log the index of the clicked square to the console
       console.log(`Square clicked: index ${index}`);
     });
   });
   
+  // Function to remove 'highlight' class from all squares
   function clearHighlights() {
     document.querySelectorAll('.square').forEach(square => {
       square.classList.remove('highlight');
